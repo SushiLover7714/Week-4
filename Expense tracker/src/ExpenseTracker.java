beec1f6b36dd6e994f91c103f909ceee7179a539
SushiLover7714 <138302226+SushiLover7714@users.noreply.github.com>
2023-10-19 21:13:19 +0630

    add income

--- Expense tracker/src/ExpenseTracker.java
+++ Expense tracker/src/ExpenseTracker.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+
 public class ExpenseTracker {
     String userDataFilePath;
     String expenseRecordsFilePath;
@@ -9,8 +22,27 @@ public class ExpenseTracker {
         this.incomeRecordsFilePath = incomeRecordsFilePath;
         User user = new User(userDataFilePath, expenseRecordsFilePath, incomeRecordsFilePath);
     }
-    public int addIncome(float amount,String source,User user){
-        
-        return 0;
+
+    public int addIncome(float amount, String source) {
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+
+        // Define the date format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        // Format the current date as a string
+        String dateString = currentDate.format(formatter);
+        String newIncomeRecordLine = source + ":" + String.valueOf(amount) + "," + dateString;
+        try {
+            FileWriter fileWriter = new FileWriter(incomeRecordsFilePath);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            writer.append(newIncomeRecordLine);
+
+            return 0;
+        }
+
+        catch (IOException e) {
+            return 1;
+        }
     }
 }
--- Expense tracker/src/ExpenseTrackerMain.java
+++ Expense tracker/src/ExpenseTrackerMain.java
@@ -8,15 +8,51 @@ import java.util.ArrayList;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;;
+import java.util.Scanner;
 
 public class ExpenseTrackerMain {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int option;
         String userDataFilePath = ".\\Expense tracker\\data\\usr_data.txt";
         String expenseRecordsFilePath = ".\\Expense tracker\\data\\expense_records.txt";
         String incomeRecordsFilePath = ".\\Expense tracker\\data\\income_record.txt";
         ExpenseTracker tracker = new ExpenseTracker(userDataFilePath, expenseRecordsFilePath, incomeRecordsFilePath);
+        System.out.println("1. Add income: ");
+        System.out.println("2. Add expense: ");
+        System.out.println("3. Check bank statistics: ");
+        System.out.println("4. Exit: ");
+        System.out.print("Enter option: ");
+        option = input.nextInt();
+        switch (option) {
 
+            case 1:
+                input.nextLine();
+                System.out.print("Enter amount: ");
+                float amount = input.nextFloat();
+                System.out.print("Enter source");
+                String source = input.nextLine();
+                int status = tracker.addIncome(amount, source);
+                if (status == 0) {
+                    System.out.println("Operation Successful!");
+                } else if (status == 1) {
+                    System.out.println("Operation not successful");
+                }
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+        }
     }
 
 }
